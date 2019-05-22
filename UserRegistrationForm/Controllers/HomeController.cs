78cbe7d27b648ddb84e4996f988a99e16a5dc59e
Your Name <n8jefferis@gmail.com>
2019-05-22 08:57:02 -0400

    first commit

---
+++ UserRegistrationForm/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRegistrationForm.Models;
+
+
+namespace UserRegistrationForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            ViewBag.Message = "Your registration page.";
+            return View();
+        }
+
+        public ActionResult Result(User inputUser)
+        {
+            ViewBag.userName = inputUser.userName;
+            return View();
+        }
+    }
+}
